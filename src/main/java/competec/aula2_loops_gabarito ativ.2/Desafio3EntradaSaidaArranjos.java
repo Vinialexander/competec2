be6958bf62d8a42a87a58a7d0433c9d6f08d5d5d
Vinialexander <167623557+Vinialexander@users.noreply.github.com>
2024-06-22 13:57:57 +0000

    salva

---
+++ src/main/java/competec/aula2_loops_gabarito ativ.2/Desafio3EntradaSaidaArranjos.java
@@ -0,0 +1,31 @@
+package competec.aula3_arranjos;
+
+import java.util.Scanner;
+
+public class Desafio3EntradaSaidaArranjos {
+
+	public static void main(String[] args) {
+
+		// Coletar tamanho arranjo
+
+		// Preencher arranjo
+
+		// Impremir arranjo
+
+		/*
+		 * Desafio extra:
+		 * Crie outro arranjo e inverta a ordem dos valores do primeiro arranjo.
+		 */
+
+		 	Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o numero de arranjos");
+		int num = scanner.nextInt();
+
+        int valor [] = new int [num];
+
+        for(int x=0;x<num;i++) {
+            System.out.println(x);
+        }
+	}
+}
